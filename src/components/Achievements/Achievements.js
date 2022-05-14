e9bca2eed27d2d38dacfe5bde234e1936de8722a
Simran Gangwani <simrangngwani3107@gmail.com>
2022-05-14 15:50:00 +0530

    docs: changed class to className

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -7,38 +7,41 @@ import medal_teal from "../../assests/medal_teal.png";
 const Achievements = () => {
   return (
     <>
-      <div class="card_container profile_container_half">
-        <div class="leaderboard_heading">
-          <div class="popular_content">
-            <h3 class="profile_page_card_heading">Achievements</h3>
-            <span class="achivement_txt_badge">38</span>
+      <div className="card_container profile_container_half">
+        <div className="leaderboard_heading">
+          <div className="popular_content">
+            <h3 className="profile_page_card_heading">Achievements</h3>
+            <span className="achivement_txt_badge">38</span>
           </div>
-          <div class="progress_div show_flex align_center">
-            <p class="mr_sm grey_text">38/50</p>
-            <div class="progress progress_2">
-              <div class="progress_done progress_done_2" data-done="80"></div>
+          <div className="progress_div show_flex align_center">
+            <p className="mr_sm grey_text">38/50</p>
+            <div className="progress progress_2">
+              <div
+                className="progress_done progress_done_2"
+                data-done="80"
+              ></div>
             </div>
           </div>
         </div>
 
-        <div class="achievement_content">
-          <div class="medals">
-            <img src={medal_blue} alt="" class="medal_img" />
-            <div class="medal_content">Comeback</div>
+        <div className="achievement_content">
+          <div className="medals">
+            <img src={medal_blue} alt="" className="medal_img" />
+            <div className="medal_content">Comeback</div>
           </div>
 
-          <div class="medals">
-            <img src={medal_purple} alt="" class="medal_img" />
-            <div class="medal_content">Lucky</div>
+          <div className="medals">
+            <img src={medal_purple} alt="" className="medal_img" />
+            <div className="medal_content">Lucky</div>
           </div>
 
-          <div class="medals">
-            <img src={medal_teal} alt="" class="medal_img" />
-            <div class="medal_content">Leading</div>
+          <div className="medals">
+            <img src={medal_teal} alt="" className="medal_img" />
+            <div className="medal_content">Leading</div>
           </div>
         </div>
 
-        <div class="achievement_card_footer grey_text">View all</div>
+        <div className="achievement_card_footer grey_text">View all</div>
       </div>
     </>
   );
--- src/components/UserProfileCard/UserProfileCard.js
+++ src/components/UserProfileCard/UserProfileCard.js
@@ -5,49 +5,52 @@ import "./UserProfileCard.css";
 const UserProfileCard = () => {
   return (
     <>
-      <div class="card_container">
-        <div class="show_flex">
-          <img src={profile} alt="" class="profile_page_img" />
+      <div className="card_container">
+        <div className="show_flex">
+          <img src={profile} alt="" className="profile_page_img" />
 
-          <div class="profile_content">
+          <div className="profile_content">
             <h2>Simran Gangwani</h2>
-            <p class="grey_text">Developer & Designer</p>
+            <p className="grey_text">Developer & Designer</p>
 
-            <div class="progress_div">
-              <p class="progress_text grey_text">179/256 played</p>
-              <div class="progress progress_1">
-                <div class="progress_done progress_done_1" data-done="80"></div>
+            <div className="progress_div">
+              <p className="progress_text grey_text">179/256 played</p>
+              <div className="progress progress_1">
+                <div
+                  className="progress_done progress_done_1"
+                  data-done="80"
+                ></div>
               </div>
             </div>
 
-            <div class="status_div show_flex">
-              <div class="status_info_icon_card show_flex">
-                <div class="icon_div">
-                  <i class="profile_score_icon_user fa-solid fa-wand-magic-sparkles"></i>
+            <div className="status_div show_flex">
+              <div className="status_info_icon_card show_flex">
+                <div className="icon_div">
+                  <i className="profile_score_icon_user fa-solid fa-wand-magic-sparkles"></i>
                 </div>
-                <div class="status_content">
+                <div className="status_content">
                   <h2>27</h2>
-                  <p class="grey_text">Quiz Won</p>
+                  <p className="grey_text">Quiz Won</p>
                 </div>
               </div>
 
-              <div class="status_info_icon_card show_flex">
-                <div class="icon_div">
-                  <i class="profile_score_icon_user fa-regular fa-star-half-stroke"></i>
+              <div className="status_info_icon_card show_flex">
+                <div className="icon_div">
+                  <i className="profile_score_icon_user fa-regular fa-star-half-stroke"></i>
                 </div>
-                <div class="status_content">
+                <div className="status_content">
                   <h2>14</h2>
-                  <p class="grey_text">Highest Score</p>
+                  <p className="grey_text">Highest Score</p>
                 </div>
               </div>
 
-              <div class="status_info_icon_card show_flex">
-                <div class="icon_div">
-                  <i class="profile_score_icon_user fa-solid fa-check"></i>
+              <div className="status_info_icon_card show_flex">
+                <div className="icon_div">
+                  <i className="profile_score_icon_user fa-solid fa-check"></i>
                 </div>
-                <div class="status_content">
+                <div className="status_content">
                   <h2>158</h2>
-                  <p class="grey_text">Correct Answers</p>
+                  <p className="grey_text">Correct Answers</p>
                 </div>
               </div>
             </div>
